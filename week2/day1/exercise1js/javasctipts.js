8dcd3842886768dd73fd125073248ac2b91c38a8
royalkaz <royal@kazimli.com>
2023-09-11 18:24:05 +0400

    javascript exercisesXP tapsiriginin tamami

---
+++ week2/day1/exercise1js/javasctipts.js
@@ -0,0 +1,21 @@
+let adressNumber ="55"
+let adressStreet ="Mireli Qasqay"
+let country ="Azerbaycan"
+let a=`I live in ${country}+${adressStreet}+${adressNumber}`
+
+console.log(a)
+
+let birthYear ="1988"
+let lastYear  ="2055"
+
+let year= lastYear-birthYear
+let b=`Menim ${lastYear} ci ilde ${year} yasim olacaq`
+
+console.log(b) 
+ 
+let pets=['cat','dog','fish','rabbit','cow']
+console.log(pets)
+let pet=pets[1]
+console.log(pet)
+pets.splice(3,1,"hourse")
+console.log(pets)
---
+++ week2/day1/exerciseXp/javasctipts.js
@@ -0,0 +1,211 @@
+//exercisexp lesson1
+    let myfavoritFood =["egg"," orangejust"," pancake"]
+    let myfavoritMeal = "Day"
+    let eat =`I eat ${myfavoritFood} at every ${myfavoritMeal}.`
+    console.log(eat)
+//exercisexp lesson1
+
+//exercisexp lesson2
+    let myWatchedSeries = ["black mirror", "money heist", "the big bang theory"];
+    myWatchedSeries.Length //4
+    console.log(myWatchedSeries)
+    let myWatchedSeriesSentence= `I watched ${myWatchedSeries[0]} 1 week ago.`
+    console.log(myWatchedSeriesSentence)
+    myWatchedSeries.splice(2,1,"friends")
+    console.log(myWatchedSeries)
+    myWatchedSeries.push("Luch")
+    myWatchedSeries.unshift("fjdkfjdk")
+    myWatchedSeries.splice(1,1)
+    console.log(myWatchedSeries)
+    console.log(myWatchedSeries[1].charAt(2))
+    console.log(myWatchedSeries)
+//exercisexp lesson2
+
+//exercisexp lesson3
+    let a=12
+    let b=5
+    let c=9
+    let d=32
+    let e=a/b
+    console.log(e)
+    let f=e*c
+    console.log(f)
+    let g=f+d
+    console.log(g)
+    let i=((a/b)*c)+d
+    console.log(i)
+//exercisexp lesson3
+
+//exercisexp lesson4
+    let c1;
+    let a1 = 34;
+    let b1 = 21;
+    console.log(a1+b1) //first expression
+    // Prediction:55
+    // Actual:55
+
+    a1 = 2;
+    console.log(a1+b1) //second expression
+    // Prediction:2b1
+    // Actual:2b1
+     
+    console.log(3 + 4 + '5') // third expression
+    // Prediction:75
+    // Actual:75
+//exercisexp lesson4
+
+//exercisexp lesson5
+    typeof(15)
+    console.log(typeof(15))
+    // Prediction:number
+    // Actual:number
+
+    typeof(5.5)
+    console.log(typeof(5.5))
+    // Prediction:number
+    // Actual:
+
+    typeof(NaN)
+    console.log(typeof(NaN))
+    // Prediction:number
+    // Actual:number
+
+    typeof("hello")
+    console.log(typeof(hello))
+    // Prediction:worb
+    // Actual:underfined
+
+    typeof(true)
+    console.log(typeof(true))
+    // Prediction:number
+    // Actual:boolean
+
+    typeof(1 != 2)
+    console.log(typeof(1 != 2))
+    // Prediction:
+    // Actual:boolean
+
+    "hamburger" + "s"
+    console.log("hamburger" + "s")
+    // Prediction:hamburgers
+    // Actual:hamburgers
+
+    "hamburgers" - "s"
+    console.log("hamburger" - "s")
+    // Prediction:NqN
+    // Actual:NqN
+
+    "1" + "3"
+    console.log("1"+"3")
+    // Prediction:13
+    // Actual:13
+
+    "1" - "3"
+    console.log("1"-"3")
+    // Prediction:-2
+    // Actual:-2
+
+    "johnny" + 5
+    console.log("johnny"+5)
+    // Prediction:jonny5
+    // Actual:jonny5
+
+    "johnny" - 5
+    console.log("johnny"-5)
+    // Prediction:NaN
+    // Actual:NaN
+
+    99 * "hello"
+    // Prediction:notworking code
+    // Actual:
+
+    1 != 1
+    // Prediction:notworking code
+    // Actual:
+
+    1 == "1"
+console.log(1=="1")
+    // Prediction:true        
+    // Actual:true
+
+    1 === "1"
+    console.log(1==="1")
+    // Prediction:false
+    // Actual:false
+
+//exercisexp lesson5
+
+//exercisexp lesson6
+    5 + "34"
+    console.log(5+"34")
+    // Prediction:534
+    // Actual:534
+
+    5 - "4"
+    console.log(5-"4")
+    // Prediction:1
+    // Actual:1
+
+    10 % 5
+    console.log(10%5)
+    // Prediction:0
+    // Actual:0
+
+    5 % 10
+    console.log(5%10)
+    // Prediction:0.5
+    // Actual:0
+
+    "Java" + "Script"
+    console.log("Java" + "Script")
+    // Prediction:JavaScript
+    // Actual:JavaScript
+
+    " " + " "
+    console.log(" "+" ")
+    // Prediction:bosluq(probeq)  
+    // Actual:  bosluq(probeq)
+
+    " " + 0
+    console.log(" " +0)
+    // Prediction:0
+    // Actual:0
+
+    true + true
+    console.log(true+true)
+    // Prediction:true  
+    // Actual:2
+
+    true + false
+    console.log(true + false)
+    // Prediction:1
+    // Actual:1
+
+    false + true
+    console.log(false+true)
+    // Prediction:1
+    // Actual:1
+
+    false - true
+    console.log(false-true)
+
+    // Prediction:-1
+    // Actual:-1
+
+    !true
+    console.log(!true)
+    // Prediction:false 
+    // Actual:false
+
+    3 - 4
+    console.log(3-4)
+    // Prediction:-1
+    // Actual:-1
+
+    "Bob" - "bill"
+    console.log("Bob"-"Bill")
+    // Prediction:NaN
+    // Actual:NaN
+//exercisexp lesson6
+
+
