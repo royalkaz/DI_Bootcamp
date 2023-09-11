c7cf99cb08d29a654004b4fb99067865fb246a2b
royalkaz <royal@kazimli.com>
2023-09-11 21:25:58 +0400

    ev tapsirigi bittdi

---
+++ week2/day1/DailyChallengeJSArraysMethods/javascripts.js
@@ -0,0 +1,26 @@
+//Daily Challenge: JS Arrays & Methods lesson1
+    const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+    //Remove Banana from the array.
+    let fruit = fruits.slice(1,4)
+    console.log(fruit)
+    //Sort the array in alphabetical order.
+    fruit.sort()
+    console.log(fruit)
+    //Add “Kiwi” to the end of the array.
+    fruit.push("Kiwi")
+    //Remove “Apples” from the array. Don’t use the same method as in part 1.
+    fruit.shift()
+    console.log(fruit)
+    //Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+    //At the end you should see this outcome:
+    fruit.reverse();
+    console.log(fruit)
+//Daily Challenge: JS Arrays & Methods lesson1
+
+//Daily Challenge: JS Arrays & Methods lesson2
+    const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+    let [,[,[orange]]] = moreFruits
+    console.log(orange)
+//Daily Challenge: JS Arrays & Methods lesson2
+
+
---
+++ week2/day1/exercisesXPgold/javascript.js
@@ -0,0 +1,19 @@
+// exercise xp gold lesson1
+    let sentence = ["my","favorite","color","is","blue"];
+    let makeSentence =`${sentence[0]} ${sentence[1]} ${sentence[2]} ${sentence[3]} ${sentence[4]}`
+    console.log(makeSentence)
+// exercise xp gold lesson1
+
+// exercise xp gold lesson2
+    let str1 = "mix";
+    let str2 = "pod";
+    let delstr1 = str1.slice(2)
+    let delstr2 =str2.slice(2)
+    console.log(delstr1,delstr2)
+    let delstr3 = str1.replaceAll('x','d')
+    let delstr4 = str2.replaceAll('d','x')
+    console.log(delstr3)
+    console.log(delstr4)
+    let allchange = `${delstr3} ${delstr4}`
+    console.log(allchange)
+ // exercise xp gold lesson2
